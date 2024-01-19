0b0c374929b535eedaa2a7d5336b0b8e82cf55f8
Aaron Stainback <aaron@alephz.ai>
2024-01-18 23:10:47 -0500

    Adding vosk model download

--- src/BlazorSamples.ScratchConsole/VoskProgram.cs
+++ src/BlazorSamples.ScratchConsole/VoskProgram.cs
@@ -1,16 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Demo byte buffer
+using System.IO.Compression;
 using Vosk;
 
 namespace BlazorSamples.ScratchConsole
 {
     public static class VoskProgram
     {
+        static string models = "models";
+        static string voskModels = $"{models}/vosk";
         public static void Main()
         {
+            MainAsync().Wait();
+        }
+
+        public async static Task MainAsync()
+        {
+            var model = "vosk-model-en-us-0.22";
+            var modelSpk = "vosk-model-spk-0.4";
+            await DownloadVoskModelAsync(model);
+            await DownloadVoskModelAsync(modelSpk);
             Vosk.Vosk.SetLogLevel(0);
-            VoskRecognizer rec = new VoskRecognizer(new Model("model"), 16000.0f);
-            rec.SetSpkModel(new SpkModel("model-spk"));
+            VoskRecognizer rec = new VoskRecognizer(new Model($"{voskModels}/{model}"), 16000.0f);
+            rec.SetSpkModel(new SpkModel($"{voskModels}/{modelSpk}"));
             rec.SetMaxAlternatives(0);
             rec.SetWords(true);
             using Stream source = File.OpenRead("sample.wav");
@@ -32,8 +44,29 @@ namespace BlazorSamples.ScratchConsole
                 }
 
                 Console.WriteLine(rec.FinalResult());
-                Console.WriteLine("Done");
+                Console.WriteLine("----------------------Done----------------------");
             } while (true);
+        }
+
+        static async Task DownloadVoskModelAsync(string model)
+        {
+            var modelPath = $"{voskModels}/{model}";
+            var zipFile = $"{voskModels}/{model}.zip";
+            var url = $"https://alphacephei.com/vosk/models/{model}.zip";
+            if (!Directory.Exists(modelPath))
+            {
+                Console.WriteLine($"Downloading Model {model}");
+                if (!Directory.Exists(models)) Directory.CreateDirectory(models);
+                if (!Directory.Exists(voskModels)) Directory.CreateDirectory(voskModels);
+                Directory.CreateDirectory(modelPath);
+                using var client = new HttpClient();
+                using var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsByteArrayAsync();
+                await File.WriteAllBytesAsync(zipFile, content);
+                ZipFile.ExtractToDirectory(zipFile, voskModels);
+                File.Delete(zipFile);
+            }
         }
     }
 }
--- src/BlazorSamples.ScratchConsole/WhisperProgram.cs
+++ src/BlazorSamples.ScratchConsole/WhisperProgram.cs
@@ -12,17 +12,19 @@ namespace BlazorSamples.ScratchConsole
 {
     public static class WhisperProgram
     {
+        static string models = "models";
+        static string whisperModels = $"{models}/whisper";
         public static void Main()
         {
             MainAsync().Wait();
         }
-  
+
         public static async Task MainAsync()
         {
             // We declare three variables which we will use later, ggmlType, modelFileName and mp3FileName
             var ggmlType = GgmlType.Base;
             var modelFileName = "ggml-tiny.bin";
-            var mp3FileName = "sample.mp3";
+            var wavFileName = "sample.wav";
 
             // This section detects whether the "ggml-base.bin" file exists in our project disk. If it doesn't, it downloads it from the internet
             if (!File.Exists(modelFileName))
@@ -31,7 +33,7 @@ namespace BlazorSamples.ScratchConsole
             }
 
             // This section creates the whisperFactory object which is used to create the processor object.
-            using var whisperFactory = WhisperFactory.FromPath("ggml-tiny.bin");
+            using var whisperFactory = WhisperFactory.FromPath($"{whisperModels}/{modelFileName}");
 
             // This section creates the processor object which is used to process the audio file, it uses language `auto` to detect the language of the audio file.
             using var processor = whisperFactory.CreateBuilder()
@@ -39,14 +41,14 @@ namespace BlazorSamples.ScratchConsole
                 .Build();
 
 
-            var builder = new Mp3FileReaderBase.FrameDecompressorBuilder(wf => new Mp3FrameDecompressor(wf));
-            using var reader = new Mp3FileReaderBase(mp3FileName, builder);
-            if (!File.Exists("sample.wav"))
-            {
-                WaveFileWriter.CreateWaveFile("sample.wav", reader);
-            }
+            // var builder = new Mp3FileReaderBase.FrameDecompressorBuilder(wf => new Mp3FrameDecompressor(wf));
+            // using var reader = new Mp3FileReaderBase(mp3FileName, builder);
+            // if (!File.Exists("sample.wav"))
+            // {
+            //     WaveFileWriter.CreateWaveFile("sample.wav", reader);
+            // }
 
-            using FileStream wavStream = File.OpenRead("sample.wav");
+            using FileStream wavStream = File.OpenRead(wavFileName);
             do
             {
                 // This section sets the wavStream to the beginning of the stream. (This is required because the wavStream was written to in the previous section)
@@ -63,10 +65,16 @@ namespace BlazorSamples.ScratchConsole
 
             static async Task DownloadModel(string fileName, GgmlType ggmlType)
             {
-                Console.WriteLine($"Downloading Model {fileName}");
-                using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(ggmlType);
-                using var fileWriter = File.OpenWrite(fileName);
-                await modelStream.CopyToAsync(fileWriter);
+                var filePath = $"{whisperModels}/{fileName}";
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine($"Downloading Model {fileName}");
+                    if (!Directory.Exists(models)) Directory.CreateDirectory(models);
+                    if (!Directory.Exists(whisperModels)) Directory.CreateDirectory(whisperModels);
+                    using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(ggmlType);
+                    using var fileWriter = File.OpenWrite(filePath);
+                    await modelStream.CopyToAsync(fileWriter);
+                }
             }
         }
     }
