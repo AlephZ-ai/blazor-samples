e1e1c84b94b3aedf59805138aee3decd9518f2f6
Aaron Stainback <=>
2024-01-24 03:13:38 -0500

    Working on adding text to speech

--- src/BlazorSamples.Rpc/Program.cs
+++ src/BlazorSamples.Rpc/Program.cs
@@ -1,3 +1,4 @@
+using BlazorSamples.Rpc.PlayHT.Protos.V1;
 using BlazorSamples.Rpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,13 @@ builder.AddServiceDefaults();
 // Add services to the container.
 builder.Configuration.AddUserSecrets<Program>();
 builder.Services.AddHttpClient();
+builder.Services.AddGrpcClient<Tts.TtsClient>(options =>
+{
+    options.Address = new Uri("https://prod.turbo.play.ht");
+});
+
 var app = builder.Build();
+app.Lifetime.ApplicationStopping.Register(OnShutdown);
 
 app.MapDefaultEndpoints();
 
@@ -17,3 +24,9 @@ app.MapGrpcService<TtsService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
+
+void OnShutdown()
+{
+    TtsService.ShutdownChannel();
+}
--- src/BlazorSamples.Rpc/Services/TtsService.cs
+++ src/BlazorSamples.Rpc/Services/TtsService.cs
@@ -2,30 +2,71 @@
 using Grpc.Core;
 using BlazorSamples.Rpc.PlayHT.Protos.V1;
 using Google.Protobuf;
+using Grpc.Net.Client;
 
 namespace BlazorSamples.Rpc.Services
 {
-    public class TtsService(Tts.TtsClient client, IConfiguration configuration, HttpClient authClient) : Tts.TtsBase
+    public class TtsService(IConfiguration configuration, HttpClient authClient) : Tts.TtsBase
     {
         private const string LeaseUri = "https://api.play.ht/api/v2/leases";
         private readonly string _user = configuration.GetValue<string>("playHT:user")!;
         private readonly string _key = configuration.GetValue<string>("playHT:key")!;
+        private static GrpcChannel? _channel;
+        private static Tts.TtsClient? _client;
+        private static ByteString? _lease;
+        private static readonly object _lock = new object();
         public override async Task Tts(TtsRequest request, IServerStreamWriter<TtsResponse> responseStream, ServerCallContext context)
         {
-            var authRequest = new HttpRequestMessage(HttpMethod.Post, LeaseUri);
-            authRequest.Headers.Add("X-User-Id", _user);
-            authRequest.Headers.Add("Authorization", $"Bearer {_key}");
-            var authResponse = await authClient.SendAsync(authRequest, HttpCompletionOption.ResponseHeadersRead);
-            authResponse.EnsureSuccessStatusCode();
-            var lease = await authResponse.Content.ReadAsByteArrayAsync();
-            using var ms = new MemoryStream(lease[72..]);
-            var metadata = JsonSerializer.DeserializeAsync<IDictionary<string, string>>(ms);
-            request.Lease = ByteString.CopyFrom(lease);
-            using var call = client.Tts(request, context.RequestHeaders, context.Deadline, context.CancellationToken);
-            await foreach (var response in call.ResponseStream.ReadAllAsync())
+            var ct = context.CancellationToken;
+            await EnsureClientInitialized(ct);
+            request.Lease = _lease;
+            using var call = _client!.Tts(request, context.RequestHeaders, context.Deadline, context.CancellationToken);
+            await foreach (var response in call.ResponseStream.ReadAllAsync(ct))
             {
-                await responseStream.WriteAsync(response);
+                await responseStream.WriteAsync(response, ct);
             }
+        }
+
+        public static void ShutdownChannel()
+        {
+            _channel?.Dispose();
+        }
+
+        private async Task EnsureClientInitialized(CancellationToken ct)
+        {
+            if (_client is null)
+            {
+                var (client, channel, lease) = await InitializeClient(ct);
+                lock (_lock)
+                {
+                    if (_client is null)
+                    {
+                        _client = client;
+                        _channel = channel;
+                        _lease = lease;
+                    }
+                }
+            }
+        }
+
+        private async Task<(Tts.TtsClient client, GrpcChannel channel, ByteString lease)> InitializeClient(CancellationToken ct)
+        {
+            var authRequest = new HttpRequestMessage(HttpMethod.Post, LeaseUri)
+            {
+                Headers = { { "X-User-Id", _user }, { "Authorization", $"Bearer {_key}" } }
+            };
+
+            var authResponse = await authClient.SendAsync(authRequest, HttpCompletionOption.ResponseHeadersRead, ct);
+            authResponse.EnsureSuccessStatusCode();
+            var lease = await authResponse.Content.ReadAsByteArrayAsync(ct);
+            using var ms = new MemoryStream(lease[72..]);
+            var metadata = (await JsonSerializer.DeserializeAsync<IDictionary<string, JsonElement>>(ms, JsonSerializerOptions.Default, ct))!;
+            var inferenceAddress = metadata["inference_address"].ToString();
+            var leaseByteString = ByteString.CopyFrom(lease);
+            // Can't use DI Tts.TtsClient because it needs dynamic address but channels are thread safe
+            var channel = GrpcChannel.ForAddress($"https://{inferenceAddress}");
+            var client = new Tts.TtsClient(_channel);
+            return (client, channel, leaseByteString);
         }
     }
 }
