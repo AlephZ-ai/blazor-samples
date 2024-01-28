44b444c82674eb1f0b09a3c9a95500fb233e3e37
Aaron Stainback <aaron@alephz.ai>
2024-01-28 15:29:31 -0500

    Working on tests

--- src/BlazorSamples.Ws2/Extensions.cs
+++ src/BlazorSamples.Ws2/Extensions.cs
@@ -3,14 +3,15 @@ using System.Buffers;
 using System.Net.WebSockets;
 using System.Reactive;
 using System.Runtime.CompilerServices;
-using WebSocketAsyncEnumerableReceiveResult = (System.Net.WebSockets.ValueWebSocketReceiveResult Result, System.ReadOnlyMemory<byte> Buffer);
+using WebSocketAsyncEnumerableReceiveMessage = (System.Net.WebSockets.ValueWebSocketReceiveResult Result, System.ReadOnlyMemory<byte> Buffer);
+using WebSocketAsyncEnumerableSendMessage = (System.Net.WebSockets.WebSocketMessageFlags Flags, System.ReadOnlyMemory<byte> Buffer);
 
 namespace BlazorSamples.Ws2
 {
     public static class Extensions
     {
         public const int DefaultBufferSize = 4 * 1024;
-        public static async IAsyncEnumerable<WebSocketAsyncEnumerableReceiveResult> ReceiveAsyncEnumerable(
+        public static async IAsyncEnumerable<WebSocketAsyncEnumerableReceiveMessage> ReceiveAsyncEnumerable(
             this WebSocket webSocket,
             int bufferSize,
             [EnumeratorCancellation] CancellationToken ct = default)
@@ -35,23 +36,38 @@ namespace BlazorSamples.Ws2
             }
         }
 
-        public static IAsyncEnumerable<WebSocketAsyncEnumerableReceiveResult> ReceiveAsyncEnumerable(
+        public static IAsyncEnumerable<WebSocketAsyncEnumerableReceiveMessage> ReceiveAsyncEnumerable(
             this WebSocket webSocket,
             CancellationToken ct = default) =>
         webSocket.ReceiveAsyncEnumerable(DefaultBufferSize, ct);
 
         public static async IAsyncEnumerable<Unit> SendAsyncEnumerable(
             this WebSocket webSocket,
-            IAsyncEnumerable<ReadOnlyMemory<byte>> enumerable,
+            IAsyncEnumerable<WebSocketAsyncEnumerableSendMessage> enumerable,
             WebSocketMessageType messageType = WebSocketMessageType.Text,
             [EnumeratorCancellation] CancellationToken ct = default)
         {
-            await foreach (var buffer in enumerable.WithCancellation(ct).ConfigureAwait(false))
+            await foreach (var message in enumerable.WithCancellation(ct).ConfigureAwait(false))
             {
                 if (webSocket.State != WebSocketState.Open) break;
-                await webSocket.SendAsync(buffer, messageType, WebSocketMessageFlags.None, ct);
+                await webSocket.SendAsync(message.Buffer, messageType, message.Flags, ct);
                 yield return Unit.Default;
             }
+        }
+
+        public static IAsyncEnumerable<Unit> SendAsyncEnumerable(
+            this WebSocket webSocket,
+            IAsyncEnumerable<WebSocketAsyncEnumerableSendMessage> enumerable,
+            CancellationToken ct = default) =>
+        webSocket.SendAsyncEnumerable(enumerable, WebSocketMessageType.Text, ct);
+
+        public static IAsyncEnumerable<Unit> SendAsyncEnumerable(
+            this WebSocket webSocket,
+            IAsyncEnumerable<ReadOnlyMemory<byte>> enumerable,
+            WebSocketMessageType messageType = WebSocketMessageType.Text,
+            CancellationToken ct = default)
+        {
+            return SendAsyncEnumerable(webSocket, enumerable.Select(buffer => (WebSocketMessageFlags.EndOfMessage, buffer)), messageType, ct);
         }
 
         public static IAsyncEnumerable<Unit> SendAsyncEnumerable(
