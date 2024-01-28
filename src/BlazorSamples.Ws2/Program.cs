d63f70f4eebd5d0cb609127681a648403dc0a716
Aaron Stainback <aaron@alephz.ai>
2024-01-28 02:44:50 -0500

    Simple Echo

--- src/BlazorSamples.Ws2/Program.cs
+++ src/BlazorSamples.Ws2/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.HttpOverrides;
+using System.Net.WebSockets;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,3 +30,45 @@ app.UseCors();
 app.UseWebSockets();
 app.MapGet("/", () => "Hello World!");
 app.Run();
+
+app.MapGet("/stream", async (
+    HttpContext context,
+    CancellationToken ct
+) =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        await Echo(webSocket);
+    }
+    else
+    {
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+    }
+})
+.WithName("Stream")
+.RequireCors();
+
+static async Task Echo(WebSocket webSocket)
+{
+    var buffer = new byte[1024 * 4];
+    var receiveResult = await webSocket.ReceiveAsync(
+        new ArraySegment<byte>(buffer), CancellationToken.None);
+
+    while (!receiveResult.CloseStatus.HasValue)
+    {
+        await webSocket.SendAsync(
+            new ArraySegment<byte>(buffer, 0, receiveResult.Count),
+            receiveResult.MessageType,
+            receiveResult.EndOfMessage,
+            CancellationToken.None);
+
+        receiveResult = await webSocket.ReceiveAsync(
+            new ArraySegment<byte>(buffer), CancellationToken.None);
+    }
+
+    await webSocket.CloseAsync(
+        receiveResult.CloseStatus.Value,
+        receiveResult.CloseStatusDescription,
+        CancellationToken.None);
+}
